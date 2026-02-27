0661a5d36b7167cb3a4653904812e24599dbd262
pa <pramodh.ayyappan@hotmail.com>
2026-02-27 17:23:47 +0530

    feat: Increase ASCII character set for better image quality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -167,7 +167,7 @@ export default function App() {
           <AsciiInput
             value={asciiText}
             onChange={setAsciiText}
-            placeholder="Paste ASCII art here (use characters: @%#*+=-:. )"
+            placeholder="Paste ASCII art here (use characters: $@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\&quot;^`\'. )"
           />
           <div className="controls-section">
             <div className="control-group">
